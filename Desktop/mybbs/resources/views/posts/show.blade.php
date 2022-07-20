43acc2bebbbc52168b036f67d26a6f1daf4d1a10
venom-shougo <wb.work.atelier@gmail.com>
2022-07-20 20:56:08 +0900

    <scriptで削除前にダイアログを表示>

--- Desktop/mybbs/resources/views/posts/show.blade.php
+++ Desktop/mybbs/resources/views/posts/show.blade.php
@@ -9,13 +9,28 @@
     <h1>
         <span>{{ $post->title }}</span>
         <a href="{{ route('posts.edit', $post) }}">[Edit]</a>
-        <form method="post" action="{{ route('posts.destroy', $post) }}">
+        <form method="post" action="{{ route('posts.destroy', $post) }}" id="delete_post">
             @method('DELETE')
             @csrf
-            
+
             <button class="btn">[x]</button>
         </form>
     </h1>
     <p>{!! nl2br(e($post->body)) !!}</p>
+
+    <script>
+        'use strict';
+        {
+            document.getElementById('delete_post').addEventListener('submit', e => {
+                e.preventDefault();
+
+                if (!confirm('Sure to delete?')) {
+                    return;
+                }
+
+                e.target.submit();
+            });
+        }
+    </script>
 </x-layout>
 
