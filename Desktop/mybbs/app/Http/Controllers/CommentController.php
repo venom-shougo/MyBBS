87dc32eba59d76882695c52a6fd8d526452b2b6a
venom-shougo <wb.work.atelier@gmail.com>
2022-07-29 21:00:36 +0900

    <コメント追加機能実装３ DB処理 リダイレクト>

--- Desktop/mybbs/app/Http/Controllers/CommentController.php
+++ Desktop/mybbs/app/Http/Controllers/CommentController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post; //Post 名前空間
+use App\Models\Comment; //Comment 名前空間
 
 class CommentController extends Controller
 {
-    public function store(Request $request, Post $post)
+    public function store(Request $request, Post $post) //ストアメソッド
     {
-        
+        $comment = new Comment(); //commentモデル インスタンス
+        $comment->post_id = $post->id; //外部キー制約でコメントするpostのidを持ってくる
+        $comment->body = $request->body; //リクエストで渡ってきたbodyを使う
+        $comment->save();
+
+        return redirect() //投稿の詳細ページにリダイレクト
+            ->route('posts.show', $post);
     }
 }
